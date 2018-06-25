ee6db0a135bf8658853e60a87b819fa2071445b1
Eric Gentry <ericgentry@Erics-MacBook-Pro.local>
2018-06-24 22:10:01 -0700

    Added a bunch of new sections and text
    - focused on new format
    - left the items written before at the bottom of the file
    - Abstract is fully written (and needs review)
    - Literature Review is organized, but not complete
    - added appendix for creating SEAKER (need more pictures)
    - added appendix for using SEAKER (need more pictures and info)

---
+++ config.sh
@@ -0,0 +1,11 @@
+# CONFIGURATION SETTINGS
+# Raspberry Pi Password
+PI_PASSWORD="raspberry"
+# WiFi Network Name
+WIFI_NAME="SEAKER01"
+# WiFi Network Password:
+WIFI_PASSWORD="raspberry"
+# IP address which is used to access SEAKER web page
+WIFI_ROUTER_IP="192.168.101.1"
+# DHCP Range (How many connections can be made simultaneously)
+WIFI_ROUTER_DHCP_RANGE="192.168.101.50 192.168.101.100”
